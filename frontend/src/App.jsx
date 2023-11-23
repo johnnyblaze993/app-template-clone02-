55b3c96b51e99211328cad7b3872d909480cb425
johnnyblaze993 <john.alvarez.93@gmail.com>
2023-11-23 15:20:05 -0500

    Refactor code and update styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,29 @@
+import { Box } from "@mui/material";
 import "./App.css";
 import Digimon from "./components/Digimon";
 
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 // import Pokemon from "./components/Pokemon";
 import PokemonList from "./components/PokemonList";
 
 function App() {
 	return (
-		<>
-			<Nav />
+		<Box
+			sx={{
+				bgcolor: "background.paper",
+				border: "2px solid #000",
+				boxShadow: 24,
+				p: 4,
+			}}
+		>
+			{/* <Nav /> */}
 			<h2 style={{ textAlign: "center", marginBottom: "20px" }}>
 				PKM APP BOI!!!
 			</h2>
 			<Digimon />
 
 			<PokemonList />
-		</>
+		</Box>
 	);
 }
 
--- frontend/src/components/PokemonList.jsx
+++ frontend/src/components/PokemonList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import InfiniteScroll from "react-infinite-scroll-component";
 import PokemonDetails from "./PokemonDetails";
@@ -11,7 +11,7 @@ const style = {
 	top: "50%",
 	left: "50%",
 	transform: "translate(-50%, -50%)",
-	width: "80%",
+	width: "100%",
 	maxHeight: "80%",
 	overflow: "auto",
 	bgcolor: "background.paper",
