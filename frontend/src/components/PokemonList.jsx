3d004a3568ddab050adefbad7765311572a9df67
johnnyblaze993 <john.alvarez.93@gmail.com>
2023-11-24 13:36:00 -0500

    Refactor PokemonList component to use Material-UI Accordion

--- frontend/src/components/PokemonList.jsx
+++ frontend/src/components/PokemonList.jsx
@@ -2,23 +2,15 @@ import { useState } from "react";
 import axios from "axios";
 import InfiniteScroll from "react-infinite-scroll-component";
 import PokemonDetails from "./PokemonDetails";
-import Modal from "@mui/material/Modal";
-import Box from "@mui/material/Box";
+
 import LinearProgress from "@mui/material/LinearProgress";
 
-const style = {
-	position: "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: "80%",
-	maxHeight: "80%",
-	overflow: "auto",
-	bgcolor: "background.paper",
-	border: "2px solid #000",
-	boxShadow: 24,
-	p: 4,
-};
+//accordion
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const PokemonList = () => {
 	const [pokemons, setPokemons] = useState([]);
@@ -28,9 +20,8 @@ const PokemonList = () => {
 	const [open, setOpen] = useState(false);
 	const [progress, setProgress] = useState(0);
 	const [currentRegion, setCurrentRegion] = useState(null);
+	const [expandedPanel, setExpandedPanel] = useState(null);
 
-	// TODO seperate out the fetch so that we can reuse for other regions
-	// TODO create a variable for the number of pokemon in each region so we know when to stop. this can be  aconstant in its own file really. but keep in here for now.
 	// TODO create tests for this component
 
 	const REGION_RANGES = {
@@ -39,162 +30,105 @@ const PokemonList = () => {
 		Hoenn: { start: 252, end: 386 },
 	};
 
-	const fetchRegionPokemons = async (region) => {
+	//accordion
+	const handleAccordionChange = (panel) => (event, isExpanded) => {
+		if (isExpanded) {
+			setCurrentRegion(panel);
+			setExpandedPanel(panel); // Set the currently expanded panel
+			setPokemons([]); // Reset current pokemons
+			setOffset(REGION_RANGES[panel].start - 1); // Set offset to the start of the region
+			setHasMore(true); // Reset hasMore to true
+			setProgress(0); // Reset progress
+			fetchRegionPokemons(panel, REGION_RANGES[panel].start - 1, []); // Fetch pokemons for the selected region
+		} else {
+			setExpandedPanel(null); // Close all panels
+			setCurrentRegion(null);
+		}
+	};
+
+	const fetchRegionPokemons = async (
+		region,
+		currentOffset,
+		currentPokemons
+	) => {
 		const regionRange = REGION_RANGES[region];
 		const totalPokemonInRegion = regionRange.end - regionRange.start + 1;
-		const alreadyFetched = pokemons.length;
 
-		if (alreadyFetched >= totalPokemonInRegion) {
-			setHasMore(false);
-			return;
-		}
+		// Reset loading state if it's a new region
+		if (currentPokemons.length === 0) setLoading(true);
 
-		setLoading(true);
+		const limit = Math.min(10, totalPokemonInRegion - currentPokemons.length);
+		const calculatedOffset = regionRange.start - 1 + currentPokemons.length;
+
 		try {
-			const limit = Math.min(10, totalPokemonInRegion - alreadyFetched);
-			// Calculate offset based on the region's starting point
-			const calculatedOffset = regionRange.start - 1 + alreadyFetched;
-
 			const response = await axios.get(
 				`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${calculatedOffset}`
 			);
 			setPokemons((prev) => [
-				...prev,
+				...currentPokemons, // Use the passed currentPokemons, not the possibly outdated prev
 				...response.data.results.map((item) => ({
 					name: item.name,
 					url: item.url,
 				})),
 			]);
 			setOffset(calculatedOffset + limit);
-			setProgress(((alreadyFetched + limit) / totalPokemonInRegion) * 100);
+			setProgress(
+				((currentPokemons.length + limit) / totalPokemonInRegion) * 100
+			);
+
+			// If after fetching new data we've reached the end, set hasMore to false
+			if (currentPokemons.length + limit >= totalPokemonInRegion) {
+				setHasMore(false);
+			}
 		} catch (error) {
 			console.error("Error fetching data: ", error);
+		} finally {
+			setLoading(false);
 		}
-		setLoading(false);
-	};
-
-	const handleRegionClick = (region) => {
-		setPokemons([]);
-		setProgress(0);
-		setOpen(true);
-		setCurrentRegion(region);
-		fetchRegionPokemons(region);
 	};
 
 	const fetchMoreData = () => {
-		if (currentRegion) {
+		if (currentRegion && hasMore) {
 			fetchRegionPokemons(currentRegion, offset, pokemons);
 		}
 	};
 
-	const handleModalClose = () => {
-		// Resetting states when modal is closed
-		setOpen(false);
-		setPokemons([]);
-		setOffset(0);
-		setProgress(0);
-		setCurrentRegion(null);
-		setHasMore(true);
-	};
-	// const fetchPokemons = async () => {
-	// 	if (pokemons.length >= 151) {
-	// 		setHasMore(false);
-	// 		return;
-	// 	}
-
-	// 	setLoading(true);
-	// 	try {
-	// 		const limit = Math.min(10, 151 - pokemons.length);
-	// 		const response = await axios.get(
-	// 			`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-	// 		);
-	// 		setPokemons((prev) => [
-	// 			...prev,
-	// 			...response.data.results.map((item) => ({
-	// 				name: item.name,
-	// 				url: item.url,
-	// 			})),
-	// 		]);
-	// 		setOffset((prevOffset) => prevOffset + limit);
-	// 		// Update progress
-	// 		setProgress(((pokemons.length + limit) / 151) * 100);
-	// 	} catch (error) {
-	// 		console.error("Error fetching data: ", error);
-	// 	}
-	// 	setLoading(false);
-	// };
-
-	// const handleKantoClick = () => {
-	// 	setOpen(true);
-	// 	if (pokemons.length === 0) {
-	// 		fetchPokemons();
-	// 	}
-	// };
-
-	// const fetchMoreData = () => {
-	// 	fetchPokemons();
-	// };
-
 	return (
 		<div>
-			{/* <button onClick={handleKantoClick}>Kanto</button> */}
 			{Object.keys(REGION_RANGES).map((region) => (
-				<button key={region} onClick={() => handleRegionClick(region)}>
-					{region}
-				</button>
-			))}
-			<Modal
-				open={open}
-				onClose={handleModalClose}
-				aria-labelledby="pokemon-modal-title"
-				aria-describedby="pokemon-modal-description"
-			>
-				<Box sx={style} id="modal-content">
-					<LinearProgress variant="determinate" value={progress} />
-					<h1 id="pokemon-modal-title">Kanto Pokédex</h1>
-					<div id="pokemon-modal-description">
+				<Accordion
+					key={region}
+					expanded={expandedPanel === region}
+					onChange={handleAccordionChange(region)}
+				>
+					<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+						<Typography>{region} Region</Typography>
+					</AccordionSummary>
+					<AccordionDetails>
+						{loading && <p>Loading...</p>}
 						<InfiniteScroll
-							dataLength={pokemons.length}
+							dataLength={pokemons.length} // This is important field to render the next data
 							next={fetchMoreData}
-							hasMore={hasMore}
-							loader={<div>Loading...</div>}
-							endMessage={
-								<p style={{ textAlign: "center" }}>
-									<b>Yay! You have seen it all</b>
-								</p>
-							}
-							scrollableTarget="modal-content"
+							hasMore={hasMore} // If there is more data to load
+							loader={<p>Loading...</p>} // Loader element
+							// You might need to adjust the scrollableTarget or other props to work with the accordion
 						>
 							{pokemons.map((pokemon, index) => (
-								<div
-									key={pokemon.name}
-									style={{
-										// display: "flex",
-										// flexDirection: "row",
-										// justifyContent: "space-between",
-										alignItems: "center",
-										borderBottom: "1px solid black",
-										paddingBottom: "5px",
-										marginBottom: "5px",
-									}}
-								>
-									<p>{index + 1}</p>
-									<p>
-										{
-											//make first letter uppercase
-											pokemon.name.charAt(0).toUpperCase() +
-												pokemon.name.slice(1)
-										}
-									</p>
+								<div key={pokemon.name} /* Add your styling here */>
+									<p>{pokemon.name}</p>
 									<PokemonDetails url={pokemon.url} />
 								</div>
 							))}
 						</InfiniteScroll>
-					</div>
-				</Box>
-			</Modal>
+						{!hasMore && (
+							<p style={{ textAlign: "center" }}>
+								<b>Yay! You have seen it all</b>
+							</p>
+						)}
+					</AccordionDetails>
+				</Accordion>
+			))}
 		</div>
 	);
 };
-
 export default PokemonList;
