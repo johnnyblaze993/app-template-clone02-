9c2c688436a2fb19c48d8986f7c388a8ac702bb4
alvarezjo <jonathan.alvarez@axientcorp.com>
2023-11-30 09:03:16 -0600

    added more tests boi!

--- frontend/src/components/PokemonList.test.jsx
+++ frontend/src/components/PokemonList.test.jsx
@@ -7,4 +7,14 @@ describe('PokemonList', () => {
     render(<PokemonList />);
     expect(screen.getByText(/Kanto Region/i)).toBeInTheDocument();
   });
+
+  it('renders Johto Region text', () => {
+    render(<PokemonList />);
+    expect(screen.getByText(/Johto Region/i)).toBeInTheDocument();
+  });
+
+  it('renders Hoenn Region text', () => {
+    render(<PokemonList />);
+    expect(screen.getByText(/Hoenn Region/i)).toBeInTheDocument();
+  });
 });
