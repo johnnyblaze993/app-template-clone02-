6940be0a4ae487d9338812835ca7839ac0dc89ea
alvarezjo <jonathan.alvarez@axientcorp.com>
2023-11-30 10:52:19 -0600

    added css support for jest file imports

--- frontend/src/components/PokemonList.test.jsx
+++ frontend/src/components/PokemonList.test.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+
+import React from "react";
 import { render, screen } from '@testing-library/react';
 import PokemonList from './PokemonList';
 
 describe('PokemonList', () => {
+  
   it('renders Kanto Region text', () => {
     render(<PokemonList />);
     expect(screen.getByText(/Kanto Region/i)).toBeInTheDocument();
